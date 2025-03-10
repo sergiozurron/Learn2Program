56409223e25fbd279de8104ce2ad5b7919040c5e
juanpf04 <juanpf04@ucm.es>
2025-03-10 19:19:35 +0100

    nuevo logo, cambios en la base de datos y primera hu estructura (realizado por todo el equipo en live share)

--- app.js
+++ app.js
@@ -11,22 +11,35 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
 // sirve para que en tiempo de ejecución el servidor sepa acceder a la carpeta public para imagenes, etc
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Ruta principal (de momento se quedará así para la primera historia de usuario)
 app.get('/', (req, res) => {
-  const query = 'SELECT * FROM TeoriaCursos';
-  pool.query(query, (err, results) => {
+  const query1 = 'SELECT * FROM Cursos';
+  pool.query(query1, (err, cursos) => {
     if (err) {
       res.status(500).send('Error al obtener los datos: ' + err.message);
       return;
     }
-    res.render('ver_teoria_curso', { cursos: results });
+
+    // Seleccionamos el primer curso de la lista ya que solo
+    // hay uno para la primera historia de usuario
+    var curso = cursos[0]; 
+
+     // Obtener los temas del curso usando el idCurso
+     const query2 = 'SELECT * FROM Temas WHERE idCurso = ?';
+     pool.query(query2, [curso.idCurso], (err, temas) => {
+        if (err) {
+         res.status(500).send('Error al obtener los temas: ' + err.message);
+         return;
+        }
+
+        res.render('ver_teoria_curso', { curso: curso, temas: temas});
+     });
   });
-});
+}); 
 
 // Iniciar el servidor en el puerto
 app.listen(port, () => {
