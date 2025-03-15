bcadcb707bb7f6e3bfe1768c15945e71421ef56b
juanpf04 <juanpf04@ucm.es>
2025-03-15 22:21:08 +0100

    prueba test

---
+++ tests/prueba.test.js
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+const fs = require("fs");
+const path = require("path");
+
+describe("Test de prueba", () => {
+
+  test("Debe existir el contenedor principal", () => {});
+
+  test("El botón de scroll debe estar oculto inicialmente", () => { });
+
+  test("Debe haber al menos un acordeón si hay temas", () => {});
+});
